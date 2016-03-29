173cdfc0dcfe9678c933372389a4e5e4a6ade988
DJ Daugherty <ddaugher@gmail.com>
2016-03-28 20:49:44 -0400

    passing test

--- src/main/scala/Character.scala
+++ src/main/scala/Character.scala
@@ -1,7 +1,11 @@
-class Character (private val _name: String) {
+class Character(private val _name: String) {
 
   val name: String = _name
   var alignment: Alignment = Neutral
   var armorClass: Int = 10
   var hitPoints: Int = 5
+
+  def attack(opponent: Character, roll: Int): Attack = {
+    Miss
+  }
 }
