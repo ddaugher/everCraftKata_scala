66977a49be5b75546fd17412ba5a90c947a2d64d
DJ Daugherty <ddaugher@gmail.com>
2016-03-28 21:28:33 -0400

    natural 20 then a critical hit is dealt and the damage is doubled

--- src/test/scala/CharacterCanBeDamagedSpec.scala
+++ src/test/scala/CharacterCanBeDamagedSpec.scala
@@ -13,8 +13,19 @@ class CharacterCanBeDamagedSpec extends FlatSpec with Matchers {
     9 should ===(opponent.hitPoints)
   }
 
+  "Character" should "incur 2 points of damage when a natural 20 is rolled" in {
+    val character = new Character("TOM")
+    val opponent = new Character("OPPONENT")
 
-//  If attack is successful, other character takes 1 point of damage when hit
+    opponent.hitPoints = 10
+    val roll: Int = 20
+
+    character.attack(opponent, roll)
+
+    8 should ===(opponent.hitPoints)
+  }
+
+
 //  If a roll is a natural 20 then a critical hit is dealt and the damage is doubled
 //    when hit points are 0 or less, the character is dead
 }
